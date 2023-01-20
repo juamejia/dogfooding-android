8e7290988789af2ee920d65cfa6ce4e00b22f55c
BypvML <bayron.pena@mercadolibre.com.co>
2023-01-19 23:40:27 -0500

    manejando excepciones en DogfoodService

--- configure/src/main/java/com/mercadolibre/android/dogfooding/configure/api/DogfoodService.kt
+++ configure/src/main/java/com/mercadolibre/android/dogfooding/configure/api/DogfoodService.kt
@@ -1,5 +1,6 @@
 package com.mercadolibre.android.dogfooding.configure.api
 
+import com.mercadolibre.android.commons.logging.Log
 import com.mercadolibre.android.restclient.RepositoryFactory
 import retrofit2.Response
 import retrofit2.http.GET
@@ -9,9 +10,14 @@ interface DogfoodService {
     suspend fun validate(): Response<Unit>
 }
 
-internal fun dogfoodService(): DogfoodService =
-    RepositoryFactory.newBuilder(BASE_URL)
-        .create(DogfoodService::class.java)
-
+internal fun dogfoodService(): DogfoodService {
+    try {
+        return RepositoryFactory.newBuilder(BASE_URL)
+            .create(DogfoodService::class.java)
+    } catch (e: Exception) {
+        Log.e("DogfoodService", "Error al crear la instancia de DogfoodService", e)
+        throw e
+    }
+}
 
 private const val BASE_URL = "https://api.mercadolibre.com/public/dogfooding/"
