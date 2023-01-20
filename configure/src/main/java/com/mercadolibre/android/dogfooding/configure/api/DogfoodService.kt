57dc987fddbd9e254db24072e78587024f529e23
BypvML <bayron.pena@mercadolibre.com.co>
2023-01-19 23:46:12 -0500

    update

--- configure/src/main/java/com/mercadolibre/android/dogfooding/configure/api/DogfoodService.kt
+++ configure/src/main/java/com/mercadolibre/android/dogfooding/configure/api/DogfoodService.kt
@@ -1,6 +1,5 @@
 package com.mercadolibre.android.dogfooding.configure.api
 
-import com.mercadolibre.android.commons.logging.Log
 import com.mercadolibre.android.restclient.RepositoryFactory
 import retrofit2.Response
 import retrofit2.http.GET
@@ -11,13 +10,8 @@ interface DogfoodService {
 }
 
 internal fun dogfoodService(): DogfoodService {
-    try {
-        return RepositoryFactory.newBuilder(BASE_URL)
-            .create(DogfoodService::class.java)
-    } catch (e: Exception) {
-        Log.e("DogfoodService", "Error al crear la instancia de DogfoodService", e)
-        throw e
-    }
+    return RepositoryFactory.newBuilder(BASE_URL)
+        .create(DogfoodService::class.java)
 }
 
 private const val BASE_URL = "https://api.mercadolibre.com/public/dogfooding/"
