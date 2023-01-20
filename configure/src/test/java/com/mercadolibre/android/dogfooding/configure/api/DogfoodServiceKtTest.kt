0eeb7fb1de532ce423de5dbc70240f59704c8b7e
BypvML <bayron.pena@mercadolibre.com.co>
2023-01-20 14:18:35 -0500

    remove ExampleActivity class

--- configure/src/test/java/com/mercadolibre/android/dogfooding/configure/api/DogfoodServiceKtTest.kt
+++ configure/src/test/java/com/mercadolibre/android/dogfooding/configure/api/DogfoodServiceKtTest.kt
@@ -1,10 +1,14 @@
 package com.mercadolibre.android.dogfooding.configure.api
 
+import android.annotation.SuppressLint
+import com.mercadolibre.android.restclient.RepositoryFactory
 import io.mockk.*
+import io.mockk.every
 import junit.framework.Assert.assertEquals
 import junit.framework.Assert.assertTrue
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Before
 import org.junit.Test
 import retrofit2.Response
 import java.io.IOException
@@ -13,6 +17,8 @@ import kotlin.system.measureTimeMillis
 class DogfoodServiceKtTest {
 
     private val dogfoodService: DogfoodService = mockk()
+    private val repositoryFactory: RepositoryFactory = mockk()
+    private val baseUrl = "https://api.mercadolibre.com/public/dogfooding/"
 
     @Test
     fun `validate should make a GET request to the correct endpoint`() = runBlocking {
@@ -43,4 +49,5 @@ class DogfoodServiceKtTest {
         val elapsedTime = measureTimeMillis { response }
         assertTrue(elapsedTime < 1000)
     }
+
 }
